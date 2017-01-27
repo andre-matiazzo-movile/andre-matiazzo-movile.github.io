957da9c069bf2527268554eaf6e515da70e9fc9a
andré matiazzo <andre.matiazzo@movile.com>
2017-01-27 10:40:04 -0200

    Added notifications to frontend

--- messaging/js/forms.js
+++ messaging/js/forms.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
 
 	// DOWNLOAD BUTTON
-		$(".download-button").bind('click', function() {
-			$(this).parent().find(".download-items").fadeIn(200);
-		});
+	$(".download-button").bind('click', function() {
+		$(this).parent().find(".download-items").fadeIn(200);
+	});
 
-		// INLINE DROPDOWNs
-
+	// INLINE DROPDOWNs
 	// gives a min-width for every inline dropdown based on the longest content inside
 	var n = $(".dropdown-item-box").length;
 	$(document).find(".dropdown-item-box").each(function(){
@@ -19,45 +18,55 @@ $(document).ready(function() {
 	$(".view-box-open").removeClass("o-0");
 
 	// when clicked on the dropdown word, it opens the full box
-		$(".view-box-close").bind('click', function() {
-			$(this).next(".view-box-open").fadeIn(200);
-		});
+	$(".view-box-close").bind('click', function() {
+		$(this).next(".view-box-open").fadeIn(200);
+	});
+
 	// when an option is selected on the inline dropdown, the full box fades and the option becomes blue
-		$(".view-criteria").bind('click', function() {
-			var text = $(this).text();
-			$(this).parents(".has-dropdown").find(".insert-criteria").text(text);
-			$(this).parent().children(".view-criteria").css("color","#37474F")
-			$(this).css("color","#84ADFF")
-			$(".view-box-open").hide();
-		});
+	$(".view-criteria").bind('click', function() {
+		var text = $(this).text();
+		$(this).parents(".has-dropdown").find(".insert-criteria").text(text);
+		$(this).parent().children(".view-criteria").css("color","#37474F")
+		$(this).css("color","#84ADFF")
+		$(".view-box-open").hide();
+	});
+
 	// The full inline dropdown closes when you click anywhere but on it
-		$(document).mouseup(function (e) {
-		    var target = $(".view-box-open");
+	$(document).mouseup(function (e) {
+		var target = $(".view-box-open");
 		    if (!target.is(e.target) // if the target of the click isn't the target...
 		        && target.has(e.target).length === 0) // ... nor a descendant of the target
 		    {
-		        target.fadeOut();
+		    	target.fadeOut();
 		    }
 		});
 
 
-
-
-		// BOX DROPDOWNS
-
-	// Closes the dropdown by clicking out of it
-	$(document).bind('click', function(toggleDropdown) {
-		if (!$(toggleDropdown.target).closest('.dropdown, .input').length) {
-			$('.dropdown').removeClass('dropdown--active').animate({opacity: 0}, 15);
-		}
-	});
+	// BOX DROPDOWNS
 
 	// Closes a dropdown that is already opened if you click on another one
 	$('.input').bind('click', function (activateDropdown) {
 		$('.dropdown').removeClass('dropdown--active').animate({opacity: 0}, 15);
+		$('.upper-bar-menu').removeClass('upper-bar-menu--active');
 		$(this).parent().find('.dropdown').addClass('dropdown--active').animate({opacity: 1}, 15);
 		$(this).find('input[type="search"]').focus();
 	});
+
+		// Closes the dropdown by clicking out of it
+		$(document).bind('click', function(toggleDropdown) {
+			// If the click on the document is not a .dropdown, an .input or .upper-bar-menu--active, closes all dropdowns
+			if (!$(toggleDropdown.target).closest('.dropdown, .input, .upper-bar-menu--active').length) {
+				$('.dropdown').removeClass('dropdown--active').animate({opacity: 0}, 15);
+				$('.upper-bar-menu').removeClass('upper-bar-menu--active');
+			}
+		});
+
+		//Closes opened menu dropdowns if you click on its trigger again
+		$('.upper-bar-menu').bind('click', function () {
+			$('.dropdown').removeClass('dropdown--active').animate({opacity: 0}, 15);
+			$(this).toggleClass('upper-bar-menu--active');
+			$(this).parent().next('.dropdown').toggleClass('dropdown--active').animate({opacity: 1}, 15);
+		})
 
 	//On New Contact, shows the "improve this contact" column
 	$('.improve-contact__cta .cta').bind('click', function() {
