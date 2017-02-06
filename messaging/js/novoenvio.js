19f02b350d736b3065e656ebbffc19ed3eb56257
hudsonmacedo <hudsonmacedo14@gmail.com>
2017-02-06 11:25:35 -0200

    novo envio

--- messaging/js/novoenvio.js
+++ messaging/js/novoenvio.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	$('#schedule .radio').bind('click', function () {
-		if ($('#schedule input').is(':checked')) {
-			$('#send-message').fadeToggle();
-		}
-	});
+	// $('#schedule .radio').bind('click', function () {
+	// 	if ($('#schedule input').is(':checked')) {
+	// 		$('#send-message').fadeToggle();
+	// 	}
+	// });
 
 	$("input[type='file']").bind("change", function(){
 		$(".how-to").hide();
@@ -137,15 +137,14 @@ function bottomPreviewPosition() {
 // DONE:
 // 2. Ao escolher arquivo para importação, mostrar o nome do arquivo abaixo do botão
 // 3. Desativar busca ou importação dependendo do que a pessoa escolher. Mostrar mensagem abaixo do item inativo explicando porque ele ficou desse jeito
-
 // 6. Se pessoa escrever mensagem com acento, mudar o limite de caracteres para 70
 // 7. Se pessoa escrever mais que 160/70 caracteres, dar feedback de quantas mensagens serão cobradas
+// 9. Prévia da mensagem fica fixa depois da altura
+// 5. Dependendo do que a pessoa escolher ^ mudar o texto explicativo em "Escreva sua mensagem". Os campos podem vir do arquivo ou dos contatos/grupos selecionados
 
 // TO DO:
 
 // 4. Modais de "veja como fazer" e a tooltip do Flash SMS
-// 5. Dependendo do que a pessoa escolher ^ mudar o texto explicativo em "Escreva sua mensagem". Os campos podem vir do arquivo ou dos contatos/grupos selecionados
 
 // 8. Reconhecer onde está o caret no textarea e conseguir adicionar os campos customizados ali. Tentar fazer isso com seleção também. (talvez tenha que mudar para contenteditable)
 
-// 9. Prévia da mensagem fica fixa depois da altura
