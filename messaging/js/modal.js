61ea935961ac8bec25799e1eb49f59c320b55dc1
andré matiazzo <andre.matiazzo@movile.com>
2017-02-08 23:04:29 -0200

    Modals created

---
+++ messaging/js/modal.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	// Opens Modal
+	$('.modal-trigger').on('click', function() {
+		$('.modal').show();
+		setTimeout(function(){
+			$('.modal').animate({opacity: 1});
+		}, 150);
+	// Shows different types of content on modal
+		//How to format files
+		if ($(this).hasClass('trigger--how-to-do')) {
+			$('.modal--how-to-do').show();
+		} else if ($(this).hasClass('trigger--flash-sms')) {
+			$('.modal__body').hide();
+			$('.modal--flash-sms').show();
+		}
+	});
+
+	// Closes modal
+	$('.close-modal, .modal').on('click', function(closesModal) {
+		if (!$(closesModal.target).not('.close-modal').closest('.modal__body').length) {
+			$('.modal').animate({opacity: 0}, 15);
+			setTimeout(function(){
+				$('.modal').fadeOut();
+			}, 400);
+		}
+	});
+
+});
