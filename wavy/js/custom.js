7ffa9bc14e2f44ad2da84f555f8ae3c30b71a8a4
André Matiazzo <andre.matiazzo@movile.com>
2017-08-23 17:08:13 -0300

    minor fix

--- wavy/js/custom.js
+++ wavy/js/custom.js
@@ -5,15 +5,35 @@ $( document ).ready(function() {
     }
   });
 
+  // Open and close mobile menu
   $('#menu-open').on('click', function () {
     $('#menu__container').fadeIn(150);
     $(this).hide();
     $('#menu-close').show();
   });
 
-    $('#menu-close').on('click', function () {
+  $('#menu-close').on('click', function () {
     $('#menu__container').fadeOut(150);
     $(this).hide();
     $('#menu-open').show();
+  });
+
+  // Scroll anchor points
+  var $root = $('html, body');
+  $('a[href$="#who"]').click(function () {
+    $root.animate({
+      scrollTop:$($.attr(this,'href')).offset().top
+    }, 500);
+    return false;
+  });
+
+  // Accordions
+  $('.accordion h3').on('click', function() {
+    $('.accordion h3').removeClass('black');
+    $('.accordion h3').addClass('gray');
+    $('.accordion p').slideUp();
+    $(this).parent().find('p').slideToggle();
+    $(this).removeClass('gray');
+    $(this).addClass('black');
   });
 });
